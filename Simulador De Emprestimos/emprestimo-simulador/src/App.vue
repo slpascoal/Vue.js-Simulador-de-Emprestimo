9517c2bb07c05fd5b7a2192a49de15ff8cce169c
Silas L. Pascoal <slpascoal01@gmail.com>
2024-08-05 00:18:44 -0300

    Finalização do Projeto

--- Simulador De Emprestimos/emprestimo-simulador/src/App.vue
+++ Simulador De Emprestimos/emprestimo-simulador/src/App.vue
@@ -14,3 +14,10 @@ export default {
   }
 }
 </script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+</style>
--- Simulador De Emprestimos/emprestimo-simulador/src/components/EmprestimoSimulador.vue
+++ Simulador De Emprestimos/emprestimo-simulador/src/components/EmprestimoSimulador.vue
@@ -1,45 +1,55 @@
 <template>
-  <div>
+  <div class="bodyApp">
     <h1>Simulador de Empréstimo</h1>
-    <div>
-      <label for="valor">Valor do Empréstimo</label>
-      <input v-model="valor" id="valor" type="text" />
+    <div class="form">
+      <div class="valorEmp">
+        <label for="valor">Valor do Empréstimo: </label>
+        <span>R$<input v-model="valor" id="valor" type="text" /></span>
+      </div>
+
+      <Multiselect
+        v-model="instituicaoSelecionada"
+        :options="instituicoes"
+        label="valor"
+        placeholder="Selecione uma instituição"
+      />
+
+      <Multiselect
+        v-model="convenioSelecionado"
+        :options="convenios"
+        label="valor"
+        placeholder="Selecione um convênio"
+      />
+
+      <div>
+        <label for="parcelas">Parcelas</label>
+        <select v-model="parcelasSelecionadas" id="parcelas">
+          <option v-for="parcela in opcoesParcelas" :key="parcela" :value="parcela">
+            {{ parcela === 0 ? 'Selecionar parcelas' : parcela }}
+          </option>
+        </select>
+      </div>
+
+      <button @click="simularEmprestimo">Simular</button>
+    
     </div>
+    
 
-    <Multiselect
-      v-model="instituicaoSelecionada"
-      :options="instituicoes"
-      label="valor"
-      placeholder="Selecione uma instituição"
-    />
-
-    <Multiselect
-      v-model="convenioSelecionado"
-      :options="convenios"
-      label="valor"
-      placeholder="Selecione um convênio"
-    />
-
-    <div>
-      <label for="parcelas">Parcelas</label>
-      <select v-model="parcelasSelecionadas" id="parcelas">
-        <option v-for="parcela in opcoesParcelas" :key="parcela" :value="parcela">
-          {{ parcela === 0 ? 'Selecionar parcelas' : parcela }}
-        </option>
-      </select>
-    </div>
-
-    <button @click="simularEmprestimo">Simular</button>
-
-    <div v-if="resultadosSimulacao.length">
+    <div v-if="resultadosSimulacao.length" class="areaResul">
       <div v-for="(resultado, index) in resultadosSimulacao" :key="index" class="resultado">
-        <img :src="logos[resultado.instituicao]" alt="Logo" v-if="logos[resultado.instituicao]" />
-        <p>
-          R$ {{ resultado.valor_solicitado }} - {{ resultado.parcelas }} x R$ {{ resultado.valor_parcela }}
-        </p>
-        <p>
-          {{ resultado.instituicao }} ({{ indexParaConvenio[resultado.convenio_index] || 'Não disponível' }}) - {{ resultado.taxa_juros }}%
-        </p>
+        <div>
+          <img :src="logos[resultado.instituicao]" alt="Logo" v-if="logos[resultado.instituicao]" width="50"/>
+        </div>
+        
+        <div>
+          <p>
+            R$ {{ resultado.valor_solicitado <= 0 ? '0' :  resultado.valor_solicitado}} - {{ resultado.parcelas }} x R$ {{ resultado.valor_parcela  }}
+          </p>
+          <p>
+            
+            {{ resultado.instituicao }} - {{ resultado.taxa_juros }}%
+          </p>
+      </div>
       </div>
     </div>
   </div>
@@ -64,9 +74,9 @@ export default {
       resultadosSimulacao: [],
       indexParaConvenio: {},
       logos: {
-        Pan: require('@/assets/imagens/Bancopanlogo.png'),
-        bmg: require('@/assets/imagens/logo-bmg-nova.png'),
-        ole: require('@/assets/imagens/Ole-consignado-2.webp')
+        PAN: require('@/assets/imagens/Bancopanlogo.png'),
+        BMG: require('@/assets/imagens/logo-bmg-nova.png'),
+        OLE: require('@/assets/imagens/Ole-consignado-2.webp')
       }
     }
   },
@@ -121,9 +131,120 @@ export default {
 </script>
 
 <style scoped>
+*{
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.form{
+  margin: 20px 0px;
+  border-bottom: 2px solid #5050502c;
+  border-radius: 10px;
+  width: 80%;
+  background-color: #00000009;
+  align-self: center;
+
+  text-align: center;
+
+  
+
+  padding: 0px 20px;
+}
+
+.bodyApp{
+  display: flex;
+  flex-direction: column;
+}
+
+.bodyApp h1{
+  color: white;
+  background-color: #41b883;
+  padding: 20px;
+}
+
+.valorEmp{
+  padding: 20px;
+}
+
+.form label{
+  
+  box-sizing: border-box;
+  padding-bottom: 5px;
+  
+  font-size: 20px;
+}
+
+.valorEmp span{
+  font-size: 20px;
+}
+
+.valorEmp input{
+  border: 1px solid #41b883;
+  border-radius: 10px;
+  padding: 10px;
+  width: 70%;
+}
+
+.form select{
+  border: 1px solid #41b883;
+  border-radius: 10px;
+  padding: 10px;
+  width: 70%;
+  margin: 0px 10px;
+}
+
+.areaResul{
+  display: flex;
+  flex-direction: column;
+}
+
+.multiselect{
+  margin-bottom: 20px; 
+  border: 1px solid #41b883;
+  border-radius: 5px;
+}
+
+.form button{
+  margin: 20px;
+  width: 20%;
+  padding: 10px;
+
+  border: 0px solid #41b883;
+  background-color: #41b883;
+  color: white;
+  border-radius: 10px;
+  transition: 0.5s;
+}
+
+.form button:hover{
+  background-color: #41b883c5;
+  transition: 0.5s;
+  transform: scale(1.03);
+}
+
 .resultado {
-  border: 1px solid #ddd;
+  width: 75%;
+  align-self: center;
+
+  display: flex;
+  gap: 20px;
+  border-bottom: 2px solid #5050502c;
+  border-radius: 10px;
   padding: 16px;
   margin-bottom: 16px;
+  background-color: #00000009;
+}
+
+.resultado img {
+  margin-top: 10%;
+}
+
+.resultado p{
+  line-height: 20px;
+}
+
+.resultado p:first-child{
+  font-weight: bold;
 }
 </style>
